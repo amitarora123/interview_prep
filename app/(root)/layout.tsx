f5be94584d587af1cabdcf82b016a50e4d36de5c
Amit <baghlaamit06@gmail.com>
2025-04-30 23:26:08 +0530

    feat: Add user authentication check in RootLayout component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,17 @@
 import Navbar from "@/components/Navbar";
+import { getCurrentUser } from "@/lib/action/auth.action";
+import { redirect } from "next/navigation";
 
-const RootLayout = ({
+const RootLayout = async ({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) => {
+    const user = await getCurrentUser();
+    
+      if (!user){
+        redirect("/sign-in");
+      }
   return (
     <main className="root-layout">
         <Navbar />
